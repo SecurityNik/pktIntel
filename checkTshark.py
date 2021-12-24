69e0317088efada882c2c0effd887e77fe612565
SecurityNik <SecurityNik@users.noreply.github.com>
2021-12-24 15:14:45 -0500

    added support for new capturing tools
    Modified the code to add support for new capturing tools
    --------------------------------------------------
    Found the following capturing tools
    Please select a number for a capturing tool
    --------------------------------------------------
    1 : Found tshark -> True -> /usr/bin/tshark 
    2 : Found tcpdump -> True -> /usr/bin/tcpdump 
    3 : Found dumpcap -> True -> /usr/bin/dumpcap

--- checkTshark.py
+++ checkTshark.py
@@ -4,6 +4,7 @@
 Author: Nik Alleyne
 Author Blog: www.securitynik.com
 Date: 2020-03-10
+Updated on: Dec 24, 2021.
 File: checkTshark.py
 '''
 
@@ -12,6 +13,7 @@ __contact__ = 'nikalleyne at gmail dot com'
 __version__ = '1.0'
 __status__ = 'Development'
 __date__ = '2020-04-20'
+
 
 import configparser as cfgP
 import os
@@ -22,6 +24,7 @@ from signal import (signal, SIGINT)
 
 # Configuration file path
 config_file = './pktIntel.conf'
+
 
 # Catch CTRL+C
 def handler(signal_received, frame):
@@ -35,43 +38,88 @@ def pkt_ini():
     
     return pkt_config
 
-# Check if TShark is inatalled
-def verify_tshark_installed():
-    print('[*] Checking for a TShark installation ...')
+# Verify capture tool installed
+def verify_capture_tool_installed():
+    capture_tool = 0
+    capture_tool_info = {'tshark':[False, ""], 'tcpdump':[False, ""], 'dumpcap':[False, ""]}
+    global tool_choice
+
+    print('[*] Checking for an installed packet capturing tool, ie. tshark, tcpdump or dumpcap')
     time.sleep(2)
+
     check_tshark = sp.run(['which', 'tshark'], stdout=sp.PIPE)
     if ( check_tshark.returncode == 0):
-        tshark_path = str(check_tshark.stdout).replace("b'", '').replace("\\n'", '')
-        print(' [*] Found tshark: {}'.format(tshark_path))
-    else:
-        print('[*] TShark not found!')
-        print('[*] Exiting!')
-        sys.exit(-1)
+        capture_tool_info['tshark'][0] = True
+        capture_tool_info['tshark'][1] = str(check_tshark.stdout).replace("b'", '').replace("\\n'", '')
+
+    check_tcpdump = sp.run(['which', 'tcpdump'], stdout=sp.PIPE)
+    if ( check_tcpdump.returncode == 0):
+        capture_tool_info['tcpdump'][0] = True
+        capture_tool_info['tcpdump'][1] = str(check_tcpdump.stdout).replace("b'", '').replace("\\n'", '')
+
+    check_dumpcap = sp.run(['which', 'dumpcap'], stdout=sp.PIPE)
+    if ( check_tcpdump.returncode == 0):
+        capture_tool_info['dumpcap'][0] = True
+        capture_tool_info['dumpcap'][1] = str(check_dumpcap.stdout).replace("b'", '').replace("\\n'", '')
+
+    print("-" * 50)
+    print("Found the following capturing tools")
+    print("Please select a number for a capturing tool")
+    print("-" * 50)
+
+    for key in capture_tool_info:
+        capture_tool += 1
+        print(f'{capture_tool} : Found { key } -> { capture_tool_info[key][0] } -> {capture_tool_info[key][1]} ')
+
+    # Read which tool the user would like to capture with
+    tool_choice = int(input("\n[*] Please select a number: "))    
+
+
+def tshark():
+    print('\033[1;31;40m [*] Press CTRL+C to exit \033[1;31;0m')
+    print(f'[*] Capturing with tshark ...')
+    while True:
+        sp.call(['tshark', '--interface', 'any', '-w', pcap_dir + 'securitynik.pcap', '--ring-buffer', '--files:100', '--ring-buffer', 'filesize:100000', '--color', '--print'])
+
+def tcpdump():
+    print('\033[1;31;40m [*] Press CTRL+C to exit \033[1;31;0m')
+    print(f'[*] Capturing with tcpdump ... ...')
+    while True:
+        sp.call(['tcpdump', '--interface', 'any', '-w', pcap_dir + 'securitynik.pcap', '-vv', '--print', '--number', '-C', '100', '-W', '100'])
+
+def dumpcap():
+    print('\033[1;31;40m [*] Press CTRL+C to exit \033[1;31;0m')
+    print(f'[*] Capturing with dumpcap ...')
+    while True:
+        sp.call(['dumpcap', '--interface', 'any', '-w', pcap_dir + 'securitynik.pcap', '--ring-buffer', '--files:100', '--ring-buffer', 'filesize:100000','filename'])
+
 
 # start TShark
-def start_tshark():
-    # Check for main section
-    if (pkt_ini().has_section('MAIN')):
-        print('      Main section found!')
-    else:
-        print('      Error default section not found!')
-        sys.exit(-1)
-
+def start_capturing():   
+    global pcap_dir
+    signal(SIGINT, handler)
     print('[*] Verifying PCAP directory exists in MAIN section ...')
     pcap_dir = pkt_ini()['MAIN']['pcap_dir']
     print('    PCAP directory reported as "{}"'.format(pcap_dir))
-    print('[*] Starting TShark')
+
+    '''
+    perform continuous capture for files up to size 100M (100,000 Kb)
+    Once the file gets to 100M rotate to a new pcap
+    '''
+    
+    if (tool_choice == 1):
+        tshark()
+    elif (tool_choice == 2):
+        tcpdump()
+    elif (tool_choice == 3):
+        dumpcap()
+    
+    else:
+        print(f"\n[ERROR !] Sorry { tool_choice } is an invalid choice ")
+        return
+
     print('[*] PCAPs are being written to {} folder!'.format(pcap_dir))
 
-    print('\033[1;31;40m [*] Press CTRL+C to exit \033[1;31;0m')
-    
-    signal(SIGINT, handler)
-    while True:
-        '''
-        perform continuous capture for files up to size 100M (100,000 Kb)
-        Once the file gets to 100M rotate to a new pcap
-        '''
-        sp.call(['tshark', '--interface', 'any', '-w', pcap_dir + 'securitynik.pcap', '--ring-buffer', '--files:100', '--ring-buffer', 'filesize:100000', '--color', '--print'], stderr=sp.PIPE)
 
 def main():
     sp.call('clear')
@@ -81,10 +129,8 @@ def main():
     print(' Author: Nik Alleyne')
     print(' Author Blog: www.securitynik.com')
     print('--------------------------||' * 2)
-
-    verify_tshark_installed()
-
-    # Check permission. I need to be run as root or with UID=0
+    
+   # Check permission. I need to be run as root or with UID=0
     print('[*] Checking your permission ...')
     time.sleep(2)
     if ( os.getuid() != 0):
@@ -93,20 +139,32 @@ def main():
         sys.exit(-1)
 
     print('[*] Running as {} with UID {} \n'.format(os.getlogin(), os.getuid()))
-    print('[*] Checking if TShark is running ... ')
-    tshark_process_running = sp.Popen('ps aux | grep tshark --only-matching', shell=True, stdout=sp.PIPE)
-    tshark_process_running = tshark_process_running.stdout.read().decode('utf-8').split('\n')
-    
-    time.sleep(2)
-    if (len(tshark_process_running) > 3):
-        print('[*] Looks like TShark may be running already ')
+
+    # Check for main section
+    if (pkt_ini().has_section('MAIN')):
+        print('      Main section found!')
     else:
-        start_ts = input('[*] TShark is currently not running, start TShark?-  YES/NO: ').upper()
-        if (start_ts == 'YES'):
-            start_tshark()
-        else:
-            print('[*] Exiting ... ')
-            sys.exit(0)
+        print('      Error default section not found!')
+        sys.exit(-1)
+    
+    verify_capture_tool_installed()
+    start_capturing()
+
+#    print('[*] Checking if TShark is running ... ')
+#    tshark_process_running = sp.Popen('ps aux | grep tshark --only-matching', shell=True, stdout=sp.PIPE)
+#    tshark_process_running = tshark_process_running.stdout.read().decode('utf-8').split('\n')
+    
+#    time.sleep(2)
+#    if (len(tshark_process_running) > 3):
+#        print('[*] Looks like TShark may be running already ')
+#    else:
+#        start_ts = input('[*] TShark is currently not running, start TShark?-  YES/NO: ').upper()
+#        if (start_ts == 'YES'):
+#            start_tshark()
+#        else:
+#            print('[*] Exiting ... ')
+    sys.exit(0)
+
         
 if __name__ == '__main__':
     main()
